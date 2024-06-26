1f26191ef057ebf5edb6a6d2a512831ba6513016
minsepar <parkj12b@github.com>
2024-06-26 04:09:59 +0000

    .

--- index.js
+++ index.js
@@ -187,7 +187,7 @@ app.message(async ({ message, client, say }) => {
         include_locale: true
       });
       // console.log(`is this user info? ${fromUser.user.locale}`);
-      var fromName = fromUser.user.profile.realname;
+      var fromName = fromUser.user.profile.real_name;
     } catch (error) {
       console.error(error);
       var fromName = "Unidentified User";
